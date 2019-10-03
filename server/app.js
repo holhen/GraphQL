1ab26acb2643c9cfbc7cdbfc307ff605cf8b6e97
holhen <holhen@gmail.com>
2019-10-03 16:32:15 +0200

    commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql")
+const schema = require("./schema/schema")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app = express();
+app.use(cors())
+
+mongoose.connect("mongodb+srv://holhen:LdXc6LEROHcN@cluster0-vpdjk.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true})
+mongoose.connection.once("open", () => {
+    console.log("connected to database")
+})
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log("Listening for requests on port 4000");
+})
