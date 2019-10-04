a651b10c8e5fb918b4a8275d38c987aae4ef3b9e
holhen <holhen@gmail.com>
2019-10-04 10:37:51 +0200

    Updates

---
+++ precache-manifest.7c452f90e50ce47342ce55abfbbfefc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6a97f4501d8fe6832d3b6c667419a0",
+    "url": "/GraphQL/index.html"
+  },
+  {
+    "revision": "e8a97e019caa166f7b93",
+    "url": "/GraphQL/static/css/main.70b10d0c.chunk.css"
+  },
+  {
+    "revision": "553efdf1c4f8ebe280a6",
+    "url": "/GraphQL/static/js/2.096c23b6.chunk.js"
+  },
+  {
+    "revision": "e8a97e019caa166f7b93",
+    "url": "/GraphQL/static/js/main.2d817eaa.chunk.js"
+  },
+  {
+    "revision": "c4e042bf6193b69335e7",
+    "url": "/GraphQL/static/js/runtime~main.24b659e9.js"
+  }
+]);
